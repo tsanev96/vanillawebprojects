bda839cc828b956abce15e8044ab7542d70a2717
tsanev96 <tsanev96@gmail.com>
2020-02-18 11:10:37 +0200

    html and css added

---
+++ 1_form_validation/script.js
@@ -0,0 +1,5 @@
+const form = document.querySelector('#form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const passowrd = document.querySelector('#password');
+const password_2 = document.querySelector('#password2');
